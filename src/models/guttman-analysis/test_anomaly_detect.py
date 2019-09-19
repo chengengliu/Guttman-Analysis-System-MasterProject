5cf80bdb64284c30ee8eb5ca748b3ac38408d861
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2019-09-19 15:04:52 +1000

    Add unit test

---
+++ src/models/guttman-analysis/test_anomaly_detect.py
@@ -0,0 +1,22 @@
+# This is the unit test for anomaly_detect file.
+# Test the performance of the functions and algorithms.
+
+import anomaly_detect
+import csv
+
+
+
+def file_read(file):
+    print()
+
+
+
+
+
+
+
+def main():
+    file_read("1")
+
+if __name__ == '__main__':
+    main()
