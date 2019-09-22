e9431ef3cbc2ee537bbbe1995f2c04952a470341
Yi <42195839+839224346@users.noreply.github.com>
2019-09-23 00:32:19 +1000

    add a test suite for module fileImport.py

--- test/__init__.py
+++ test/__init__.py
@@ -1,4 +1,5 @@
 from .test_storage import StorageTestCase
 from .test_excel_output import ExcelOutputTestCase
 from .test_guttman_analysis import GuttmanAnalysisTestCase
+from .test_file_import import FileImportTestCase
 
