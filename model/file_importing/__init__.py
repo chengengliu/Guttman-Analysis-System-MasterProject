e502308b8b9ada429e1a8ce2ae0f32a74976c33e
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2019-10-13 21:28:43 +1100

    Align With Master Update

--- model/file_importing/__init__.py
+++ model/file_importing/__init__.py
@@ -14,9 +14,9 @@ def sort_2d_array_mark(array):
             count2 = 0
             for k in range(1, len(array[0])):
                 count1 += int(array[j][k])
-                count2 += int(array[j+1][k])
+                count2 += int(array[j + 1][k])
             if count1 < count2:
-                array[j], array[j+1] = array[j+1], array[j]
+                array[j], array[j + 1] = array[j + 1], array[j]
 
     for i in range(1, len(array[0]) - 1):
         for j in range(1, len(array[0]) - i):
@@ -24,10 +24,10 @@ def sort_2d_array_mark(array):
             count2 = 0
             for k in range(1, len(array) - 1):
                 count1 += int(array[k][j])
-                count2 += int(array[k][j+1])
+                count2 += int(array[k][j + 1])
             if count1 < count2:
                 for k in range(len(array)):
-                    array[k][j], array[k][j+1] = array[k][j+1], array[k][j]
+                    array[k][j], array[k][j + 1] = array[k][j + 1], array[k][j]
 
 
 def sort_2d_array_max_mark(array):
@@ -44,9 +44,9 @@ def sort_2d_array_max_mark(array):
             count2 = 0.0
             for k in range(1, len(array[0])):
                 count1 += int(array[j][k])
-                count2 += int(array[j+1][k])
+                count2 += int(array[j + 1][k])
             if count1 < count2:
-                array[j], array[j+1] = array[j+1], array[j]
+                array[j], array[j + 1] = array[j + 1], array[j]
 
     tmp_max_mark = [0]
     for i in range(1, len(array[0])):
@@ -61,26 +61,25 @@ def sort_2d_array_max_mark(array):
     for i in range(1, len(array)):
         if array[i][1:] == tmp_max_mark[1:]:
             max_mark_cnt += 1
-    #print(max_mark_cnt)
+    # print(max_mark_cnt)
     max_mark = [0] + [sorted([array[j][i]
-                      for j in range(1, len(array))], reverse=True)[max_mark_cnt]
+                              for j in range(1, len(array))], reverse=True)[max_mark_cnt]
                       for i in range(1, len(array[0]))
                       ]
-    #print(max_mark)
+    # print(max_mark)
     for i in range(1, len(array[0])):
         for j in range(1, len(array[0]) - i):
             count1 = (1 - float(tmp_max_mark[j]) / max_mark[j]) * max_mark_cnt
-            count2 = (1 - float(tmp_max_mark[j+1]) / max_mark[j+1]) * max_mark_cnt
+            count2 = (1 - float(tmp_max_mark[j + 1]) / max_mark[j + 1]) * max_mark_cnt
             for k in range(1, len(array)):
-                count1 += float(array[k][j])/max_mark[j]
-                count2 += float(array[k][j+1])/max_mark[j+1]
-            #print(array[0][j], count1, array[0][j+1], count2)
+                count1 += float(array[k][j]) / max_mark[j]
+                count2 += float(array[k][j + 1]) / max_mark[j + 1]
+            # print(array[0][j], count1, array[0][j+1], count2)
             if count1 < count2:
-                max_mark[j], max_mark[j+1] = max_mark[j+1], max_mark[j]
-                tmp_max_mark[j], tmp_max_mark[j+1] = tmp_max_mark[j+1], tmp_max_mark[j]
+                max_mark[j], max_mark[j + 1] = max_mark[j + 1], max_mark[j]
+                tmp_max_mark[j], tmp_max_mark[j + 1] = tmp_max_mark[j + 1], tmp_max_mark[j]
                 for k in range(len(array)):
-                    array[k][j], array[k][j+1] = array[k][j+1], array[k][j]
-                    
+                    array[k][j], array[k][j + 1] = array[k][j + 1], array[k][j]
 
 
 def transpose(array):
