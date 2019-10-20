7f26082cd04e44657bdc515193f79642f6c41cf9
Zihan ZHANG <zhazz@student.unimelb.edu.au>
2019-10-20 12:38:04 +1100

    fix feedback panel

--- static/js/view/resultView.js
+++ static/js/view/resultView.js
@@ -100,12 +100,12 @@ const renderLeftPanel = data => {
         </div>
         <div class="other-pattern">
             <a href="./result.html${query1}" class="pattern-link">
-                <span class="text">Show irregular pattern</span>
+                <span class="text">Show irregular Items</span>
             </a>
         </div>
         <div class="other-pattern">
             <a href="./result.html${query2}" class="pattern-link">
-                <span class="text">Show other pattern</span>
+                <span class="text">Show other patterns</span>
             </a>
         </div>
         <div class="export-section">
@@ -124,11 +124,11 @@ const renderFeedback = (typeID, data) => {
 
     let content;
 
-    if (typeID == 0) {
+    if (typeID == 1) {
         content = `
             <li>
                 <span>Irregular Students Pattern</span>
-                <p>Irregular students are marked with yellow color, which are ${data["irregular_student"]}.</p>
+                <p>Irregular students are marked with <span class="yellow">yellow</span> color, which are ${data["irregular_student"]}.</p>
                 <p> An irregular row of ones and zeros indicates that the student has not engaged fully in the assessment.
                     The pattern is a reflection of the student’s attention to the task, rather than their skills. Therefore,
                     it is not possible to identify the student’s point of readiness to learn from the assessment and alternate
@@ -137,24 +137,21 @@ const renderFeedback = (typeID, data) => {
             </li>
 
             <li>
-                <span>Irregular Items Pattern</span>
-                <p>Irregular items are marked with blue color, which are ${data["irregular_item"]}.</p>
-                <p>If there is an irregular mixture of ones and zeros within
-                    a column, it shows the item is not an indicator of a student’s ability in the underlying construct. This may be
-                    due to ambiguity in the item, inconsistent marking, or
-                    an item that is not matched to the underlying construct.
-                    (Eg: if a maths question is written in difficult language,
-                    the question may measure reading ability, not maths
-                    ability.) It could also be an indication that the item is too
-                    difficult for the students undertaking the assessment.
-                    Items which perform like this should be removed from
-                    the current analysis, and teachers can use this information to improve their assessment tasks in the future.
-                </p>
+                <span>Width of Mixture of Ones and Zeros</span>
+                <p>Interestring areas are grouped with <span>blue</span> boxes.</p>
+                <p>The width of the zone containing a mixture of ones and zeros in an indication of the
+                reliability of the assessment. A very wide zone means that there is a lot of ‘noise’ in
+                the measure of the students’ abilities. A narrow zone indicates that the assessment is
+                a reliability of the students’ abilities in the underlying construct. ‘Noise’ could be due
+                to a number of reasons, including the following: that the students were not engaged
+                in the task, that the assessment was not administered consistently, that the assessment
+                is not of good quality or that the assessment measures more than one underlying
+                construct.</p>
             </li>
 
             <li>
-                <span>odd cells</span>
-                <p>odd cells are marked with purple color</p>
+                <span>Odd Cells</span>
+                <p>Odd cells are marked with <span class="purple">purple</span> color</p>
                 <p>odd cells in this graph are ${data["odd_cells"]}.</p>
                 <p>An unexpected greater-then-zero odd cell after the pattern has broken down
                     should also be disregarded. It is an indication of lucky
@@ -168,11 +165,11 @@ const renderFeedback = (typeID, data) => {
                 </p>
             </li>
         `;
-    } else if (typeID == 1) {
+    } else if (typeID == 0) {
         content = `
             <li>
                 <span>Irregular Items Pattern</span>
-                <p>Irregular items are marked with blue color, which are ${data["irregular_item"]}.</p>
+                <p>Irregular items are marked with <span class="green">green</span> color, which are ${data["irregular_item"]}.</p>
                 <p>If there is an irregular mixture of ones and zeros within
                     a column, it shows the item is not an indicator of a student’s ability in the underlying construct. This may be
                     due to ambiguity in the item, inconsistent marking, or
